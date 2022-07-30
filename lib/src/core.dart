670b3c7829e6bd09b12b934e0b3b211cdfe55f88
gierr7976 <gierr7976@gmail.com>
2022-07-30 07:57:45 +0300

    feat: Core

---
+++ lib/src/core.dart
@@ -0,0 +1,49 @@
+import 'package:teledart/model.dart';
+import 'package:teledart/teledart.dart';
+import 'package:teledart/telegram.dart';
+
+import 'gen/config.dart';
+
+class DjBot {
+  final Iterable<DjBotDelegate> delegates;
+
+  TeleDart? _client;
+
+  TeleDart get client => _client!;
+
+  DjBot({
+    required this.delegates,
+  });
+
+  void start() async {
+    final User me = await Telegram(BotConfig.token).getMe();
+
+    _client = TeleDart(
+      BotConfig.token,
+      Event(me.username!),
+    );
+
+    client.start();
+
+    delegates.forEach(_initDelegate);
+  }
+
+  void stop() => client.stop();
+
+  void _initDelegate(DjBotDelegate delegate) => delegate._init(this);
+}
+
+abstract class DjBotDelegate {
+  DjBot? _bot;
+
+  TeleDart get client => _bot!.client;
+
+  void _init(DjBot bot) {
+    _bot = bot;
+    init();
+  }
+
+  void init();
+
+  void dispose();
+}
